4d8319530d57a9f8789ddfcf8de25b5d99b22104
bernardtse <142848207+bernardtse@users.noreply.github.com>
2024-11-14 20:59:50 +0000

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -77,7 +77,7 @@ def predict():
         # Determine the diabetes status for each prediction
         results = []
         for prediction in predictions:
-            diabetic = "High Risk - Contact Your GP for Further Evaluation." if prediction[0] >= 1.5 else "Within Safe Range – Regular Follow-up Advised."
+            diabetic = "High Risk - Contact Your GP for Further Evaluation." if prediction[0] >= 0.5 else "Within Safe Range – Regular Follow-up Advised."
             results.append({'diabetic': diabetic, 'probability': float(prediction[0])})
 
         return jsonify(results)
